6f5f41be1975b87bdb8e9720dd8a6ca371ed0659
satriabrianydn <satriabrianyudanto16@gmail.com>
2024-06-24 21:16:54 +0700

    updated detail berita

--- lib/pages/news_detail.dart
+++ lib/pages/news_detail.dart
@@ -33,12 +33,50 @@ class NewsDetailScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
+              "Penulis: ${news.author}\nTag: ${news.tag}\nWaktu: ${news.time}",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+              ),
+            ),
+            SizedBox(height: 20),
+            Image.network(news.thumb), // Menampilkan gambar thumbnail
+            SizedBox(height: 20),
+            Text(
+              "Deskripsi:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
               news.desc,
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: GoogleFonts.poppins().fontFamily,
               ),
             ),
+            SizedBox(height: 20),
+            // TextButton(
+            //   onPressed: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //         builder: (context) => FullNewsScreen(news: news),
+            //       ),
+            //     );
+            //   },
+            //   child: Text(
+            //     'Baca Selengkapnya',
+            //     style: TextStyle(
+            //       color: Colors.green,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
